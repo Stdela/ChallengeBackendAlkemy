920ef696eff62b99a8a33eeed336e7fa2896e7e4
Santiago <delamersantiago@gmail.com>
2021-10-20 11:28:37 -0300

    Add post

---
+++ src/main/java/com/AlkemySemana1/ChallengeBackend/entities/Post.java
@@ -0,0 +1,21 @@
+
+package com.AlkemySemana1.ChallengeBackend.entities;
+
+import javax.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ *
+ * @author delam
+ */
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Post {
+
+}
